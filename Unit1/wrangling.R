ad6664e41b9d00d96ff753617c61e5a30c50563e
Jennifer Houchins <jennhouchins@gmail.com>
2021-06-20 13:19:10 -0400

    Unit 1 Independent Analysis

---
+++ Unit1/wrangling.R
@@ -0,0 +1,15 @@
+# ASSIGNMENT DESCRIPTION #####################################
+# File:         wrangling.R
+# Project:      Unit 1 Independent Analysis
+# Author:       Jennifer Houchins
+#
+# Purpose:      
+#
+# Guiding 
+# Questions:    
+#
+# Description:  
+
+# 1 PROJECT SETUP  ################################
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(pacman)
